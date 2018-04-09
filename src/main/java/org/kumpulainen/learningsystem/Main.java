ab863a4d91916dc9e16a69a891edf8cf23414a1c
coumbole <ville.kumpulainen@gmail.com>
2018-04-09 18:55:02 +0200

    Clean up code.

--- src/main/java/org/kumpulainen/learningsystem/Main.java
+++ src/main/java/org/kumpulainen/learningsystem/Main.java
@@ -3,44 +3,9 @@ package org.kumpulainen.learningsystem;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-
-        try {
-            DriverManager.registerDriver(new org.postgresql.Driver());
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException err) {
-            System.err.println("PostgreSQL jdbc driver not found.");
-            err.printStackTrace();
-            System.exit(0);
-        } catch (SQLException err) {
-            System.err.println("Couldn't register postgresql driver");
-            err.printStackTrace();
-            System.exit(0);
-        }
-
-        System.out.println("PostreSQL JDBC driver found.");
-
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/learningsystem",
-                    "learning", "FsoN7e9UTI3EpzrjzXz1oqD3Y");
-        } catch (SQLException err) {
-            err.printStackTrace();
-            System.err.println(err.getClass().getName()+": "+err.getMessage());
-            System.exit(0);
-        }
-        if (connection != null) {
-            System.out.print("Opened db successfully!");
-        } else {
-            System.out.println("Failed to make connection.");
-        }
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("learningsystem");
         EntityManager em = emf.createEntityManager();
